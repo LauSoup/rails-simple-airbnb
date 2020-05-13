5e37840628a3a8c8b3d4e55c627deb5854144342
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-13 17:44:36 +0200

    show + create

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,4 +4,18 @@ class FlatsController < ApplicationController
     @flats = Flat.all
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+  
+  def new 
+    @flat = Flat.new
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name,:address,:description,:price_per_night,:number_of_guests,:image)
+  end
+
 end
